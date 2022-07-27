d55d164c494c13e674606f7c1b7f466181dc7224
zac610 <ramas_rtl@hotmail.com>
2022-07-27 16:27:00 +0200

    Update .clang-format and NodeModel.cpp

--- NodeModel.cpp
+++ NodeModel.cpp
@@ -3,7 +3,10 @@
 #include "appInfo.h"
 
 #include <QApplication>
+#include <QFile>
 #include <QSettings>
+
+const QString CONF_FILE_SEP = ";"; // allow to configure with a CSV editor
 
 enum ColumList
 {
@@ -11,12 +14,57 @@ enum ColumList
 	COL_LAST_SEEN
 };
 
-NodeModel::NodeModel(QObject *parent)
-	: QAbstractTableModel(parent)
+enum NStatus
 {
-	QString sSettingsFile = QApplication::applicationDirPath().left(1) + APPNAME;
-	QSettings settings(QString(sSettingsFile), QSettings::IniFormat);
-	QString someValue = settings.value("some/config/key", "default value if unset").toString(); // settings.value() returns QVariant
+	DOWN,
+	UP
+};
+
+struct NodeStatus
+{
+	int id;
+	QString ip;
+	QString nodeName;
+	NStatus status;
+	unsigned cyclesNotReplying;
+	bool replied;
+
+	//	NodeStatus(const int _id, const QString &_ip, const QString &_nodeName = "") :
+	//			id(_id), ip(_ip), nodeName(_nodeName), status(DOWN), cyclesNotReplying(0), replied(false)
+	//	{
+	//		if (nodeName.isEmpty()) nodeName = ip;
+	//	}
+	NodeStatus(QString confLine) : status(DOWN), cyclesNotReplying(0), replied(false)
+	{
+		QStringList fields = confLine.split(CONF_FILE_SEP);
+		ip = fields[0];
+		if (fields.size() > 1)
+			nodeName = fields[1];
+		else
+			nodeName = "";
+	}
+};
+
+QList<NodeStatus> gNodeList;
+
+NodeModel::NodeModel(QObject *parent) : QAbstractTableModel(parent)
+{
+	QString confFile = QApplication::applicationDirPath().left(1) + APPNAME;
+	QFile file(confFile);
+	if (file.open(QIODevice::ReadOnly))
+	{
+		QTextStream in(&file);
+
+		while (!in.atEnd())
+		{
+			QString line = in.readLine();
+			// remove comments
+			QString lineClean = line.chopped(line.indexOf("#"));
+			if (!lineClean.isEmpty()) gNodeList << NodeStatus(lineClean);
+		}
+
+		file.close();
+	}
 }
 
 int NodeModel::rowCount(const QModelIndex & /*parent*/) const
